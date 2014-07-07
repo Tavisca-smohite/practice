3c101f6e0585b2fd32390a8810d2668ef3bb727a
sheetal041292 <smohite@tavisca.com>
2014-07-07 10:50:55 +0530

    Revert "Revert "sheetal""
    This reverts commit c359b2b993a6a7010710535f08c892327962c3cb.

---
+++ test1/test1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace test1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = "sheetal";
+            object obj = str as string;
+            Console.WriteLine(obj);
+            Console.ReadKey();
+        }
+    }
+}
